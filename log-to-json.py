af0fa38dc6066920674e8f77ff87f94e1d807640
imakar <iMakar@qq.com>
2018-06-07 00:07:27 +0800

    转换到csv和json

--- log-to-json.py
+++ log-to-json.py
@@ -5,13 +5,28 @@
 #   @File:  log-to-json.py
 #   @Author:
 import json
-
-if __name__ == '__main__':
-    with open("流量样本.txt",encoding="utf-8") as fp:
+import os
+def traffic2json(filename):
+    with open(filename,encoding="utf-8") as fp:
         ll=fp.readlines()
     ld=[]
     for line in ll:
         ld.append(eval(line))
-    with open("sample1.json","w",encoding="utf8") as fp:
+    with open(os.path.splitext(filename)[0]+".json","w",encoding="utf8") as fp:
         json.dump({"ld":ld},fp,ensure_ascii=False,indent=2)
+
+
+if __name__ == '__main__':
+    for each in ["a1","a2","a3","base"]:
+        traffic2json(each+".txt")
+    traffic2json("流量样本.txt")
+    traffic2json("base.txt")
+
+    # with open("流量样本.txt",encoding="utf-8") as fp:
+    #     ll=fp.readlines()
+    # ld=[]
+    # for line in ll:
+    #     ld.append(eval(line))
+    # with open("sample1.json","w",encoding="utf8") as fp:
+    #     json.dump({"ld":ld},fp,ensure_ascii=False,indent=2)
 
